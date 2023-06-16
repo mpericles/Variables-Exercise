5b2b117078b919262ccea900c761c379a4a8bdb4
mpericles <mpericles1@yahoo.com>
2023-06-16 14:47:08 -0400

    Variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        
+        // Declare and initialize variables //
+        string Name = "Moxie";
+        int months = 6;
+        char firstlet = 'M';
+        bool isabool = true;
+        double anothernumber = 234.35;
+        decimal lastnumber = 234.45m;
+
+        Console.WriteLine($"My dog's name is {Name}, She is {months} months old");
+        Console.WriteLine($"The first letter {firstlet} and it is {isabool}");
+        Console.WriteLine($"The numbers are {anothernumber} and the proper decimal is {lastnumber}");
+        Console.WriteLine("Hello, World!");
+
         }
     }
 }
